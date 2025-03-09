f03c96fbdb407bbd92822f22096646e622c6aef3
Kyle Kaczorowski <69587926+Leaseback@users.noreply.github.com>
2025-03-09 15:49:14 -0400

    Moved network simulator to its own file

---
+++ rdtp/network_simulator.py
@@ -0,0 +1,34 @@
+import random
+import socket
+import time
+
+# Constants
+PACKET_SIZE = 1024
+LOSS_PROBABILITY = 0.1
+CORRUPTION_PROBABILITY = 0.1
+REORDER_PROBABILITY = 0.1
+
+
+# Network Simulator
+class NetworkSimulator:
+    def __init__(self, listen_address, forward_address):
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.listen_socket.bind(listen_address)
+        self.forward_address = forward_address
+
+    def start(self):
+        """Start forwarding packets with simulated impairments."""
+        while True:
+            packet, addr = self.listen_socket.recvfrom(PACKET_SIZE)
+            if random.random() < LOSS_PROBABILITY:
+                print("Packet lost!")
+                continue
+            if random.random() < CORRUPTION_PROBABILITY:
+                packet = bytearray(packet)
+                packet[5] ^= 0xFF  # Corrupt data
+                packet = bytes(packet)
+                print("Packet corrupted!")
+            if random.random() < REORDER_PROBABILITY:
+                time.sleep(0.5)
+                print("Packet reordered!")
+            self.listen_socket.sendto(packet, self.forward_address)
