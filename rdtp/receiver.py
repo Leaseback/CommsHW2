b5bf0c62a244a40b484079963e75688b27106e36
Kyle Kaczorowski <69587926+Leaseback@users.noreply.github.com>
2025-03-09 15:50:30 -0400

    Receiver moved to its own file to help for part 2

---
+++ rdtp/receiver.py
@@ -0,0 +1,32 @@
+# Receiver Class
+class Receiver:
+    def __init__(self, receiver_socket, sender_address):
+        self.sock = receiver_socket
+        self.sender_address = sender_address
+        self.expected_seq_num = 0
+        self.buffer = {}
+
+    def start(self):
+        """Start receiving packets."""
+        while True:
+            packet, _ = self.sock.recvfrom(PACKET_SIZE)
+            if is_corrupted(packet):
+                print("Received corrupted packet, ignoring...")
+                continue
+
+            seq_num, _, data = parse_packet(packet)
+            if seq_num == self.expected_seq_num:
+                print(f"Received packet {seq_num}, delivering...")
+                self.sock.sendto(struct.pack("!I", seq_num), self.sender_address)
+                self.expected_seq_num += 1
+
+                while self.expected_seq_num in self.buffer:
+                    print(f"Delivering buffered packet {self.expected_seq_num}")
+                    del self.buffer[self.expected_seq_num]
+                    self.expected_seq_num += 1
+            elif seq_num > self.expected_seq_num:
+                print(f"Out of order packet {seq_num}, buffering...")
+                self.buffer[seq_num] = data
+                self.sock.sendto(struct.pack("!I", seq_num), self.sender_address)
+            else:
+                self.sock.sendto(struct.pack("!I", seq_num), self.sender_address)
